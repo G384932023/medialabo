82e63407df22bdb16b3ca48d46fda10e7b09d006
G384932023 <238493@st.takushoku-u.ac.jp>
2024-06-12 20:55:03 +0900

    as

--- kazuate.js
+++ kazuate.js
@@ -3,44 +3,55 @@ let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
-let kaisu = 4;
+let kaisu =0;
 let kiroku=0;
 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
-hantei();
-hantei();
-hantei();
-hantei();
+let b=document.querySelector('#print');
+b.addEventListener('click',hantei);
 
 // ボタンを押した後の処理をする関数 hantei() の定義
-function hantei() {
+
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  function hantei() {
+    let i=document.querySelector('input[name="seisu"]');
+    let seisu=i.value;
   
+  let yoso = Number(seisu);
+  kaisu=kaisu+1;
+kiroku=kiroku+1;
+  let p1 =document.querySelector('span#kaisu');
+p1.textContent =kaisu;
+let p2 =document.querySelector('span#answer');
+p2.textContent  =yoso;
+let p3 =document.querySelector('p#result');
+
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
-kaisu=kaisu+1;
-kiroku=kiroku+1;
-console.log(kaisu+"回目の予想:"+yoso);
+console.log=(kaisu+"回目の予想:"+yoso);
 if(kiroku<3){
   if(kotae===yoso){
-    console.log("正解です。おめでとう!");
+    p3.textContent=("正解です。おめでとう!");
     kiroku=3;
   }else if(kotae<yoso){
-    console.log("まちがい。答えはもっと小さいですよ");
+    p3.textContent=("まちがい。答えはもっと小さいですよ");
   }else{
-    console.log("まちがい。答えはもっと大きいですよ");
+    p3.textContent=("まちがい。答えはもっと大きいですよ");
   }
 }else if(kiroku===3){
   if(kotae===yoso){
-    console.log("正解です。おめでとう!");
+    p3.textContent=("正解です。おめでとう!");
   }else {
-    console.log("まちがい。残念でした答えは"+kotae+"です。");
+    p3.textContent=("まちがい。残念でした答えは"+kotae+"です。");
   }
 }else if(kiroku>3){
-  console.log("答えは"+kotae+"でした。すでにゲームは終わっています")
+  p3.textContent=("答えは"+kotae+"でした。すでにゲームは終わっています");
 }
 }
+
+
+
+
--- weather.js
+++ weather.js
@@ -1,53 +1,73 @@
-let data = {
-  coord: {
-    lon: 116.3972,
-    lat: 39.9075
-  },
-  weather: [
-    {
-      id: 803,
-      main: "Clouds",
-      description: "曇りがち",
-      icon: "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
 
-////////// 課題3-2 ここからプログラムを書こう
-console.log(coord.lon);
-for(let n of weather){
-	console.log(n);
+let div1 =document.querySelector('div#result');
+let ul = document.createElement('ul');
+div1.insertAdjacentElement('beforeend',ul);
+
+let li1=document.createElement('li');
+li1.textContent='緯度:';
+let li2=document.createElement('li');
+li2.textContent='経度:';
+let li3=document.createElement('li');
+li3.textContent='天気:';
+let li4=document.createElement('li');
+li4.textContent='最低気温:';
+let li5=document.createElement('li');
+li5.textContent='最高気温:';
+let li6=document.createElement('li');
+li6.textContent='湿度:';
+let li7=document.createElement('li');
+li7.textContent='風速:';
+let li8=document.createElement('li');
+li8.textContent='風向:';
+let li9=document.createElement('li');
+li9.textContent='都市名:';
+let b=document.querySelector('button#btn');
+b.addEventListener('click',showSelectResult);
+
+function showSelectResult(){
+  let s=document.querySelector('select#kensaku');
+  let d =s.selectedIndex;
+  let os = s.querySelectorAll('option');  
+    let o = os.item(d); 
+      let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/'+o.id+'.json';
+  
+      axios.get(url)
+          .then(showResult)   
+          .catch(showError)  
+          .then(finish);      
+          console.log(o.textContent);
+}
+function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+      data = JSON.parse(data);
   }
+  li1.textContent='緯度:'+data.coord.lon;
+  li2.textContent='緯度:'+data.coord.lat;
+  li3.textContent='天気:'+data.weather[0].description;
+  li4.textContent='最低気温:'+data.main.temp_min;
+  li5.textContent='最高気温:'+data.main.temp_max;
+  li6.textContent='湿度:'+data.main.humidity;
+  li7.textContent='風速:'+data.wind.speed;
+  li8.textContent='風向:'+data.wind.deg;
+  li9.textContent='都市名:'+data.name;
+  ul.insertAdjacentElement('beforeend',li1);
+ul.insertAdjacentElement('beforeend',li2);
+ul.insertAdjacentElement('beforeend',li3);
+ul.insertAdjacentElement('beforeend',li4);
+ul.insertAdjacentElement('beforeend',li5);
+ul.insertAdjacentElement('beforeend',li6);
+ul.insertAdjacentElement('beforeend',li7);
+ul.insertAdjacentElement('beforeend',li8);
+ul.insertAdjacentElement('beforeend',li9);
+}
+
+function showError(err) {
+  console.log(err);
+}
+
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
+
 
